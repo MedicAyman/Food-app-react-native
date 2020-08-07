14cdbff0bfff29b42dc089412363286048c556f3
ayman <ayman@aymans-MacBook-Air.local>
2020-08-07 17:30:06 +0300

    small fix

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY =
-  "FLpDnVg4J-4SpNDfdKEwPWfPNXK9sJZYbe76c_Cg67Xw79go5UWwFrCv6fPs7RGKtPBuCtfvduCU801cZs_aecF7lalxs_03EetZeUI6c1kgghIGDtXjq0XUa84qX3Yx";
+  "ENTER_YOUR_API_KEY_HERE";
 
 export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
